5ec79267b95258b831c7efe9d383dfe58dce8da5
jonathan-chien <jonathanmchien@gmail.com>
2025-06-16 16:23:38 -0500

    Add function to create seeds using numpy's SeedSequence

--- src/core_utils/reproducibility.py
+++ src/core_utils/reproducibility.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 
@@ -8,6 +9,25 @@ def set_seed(torch_seed, cuda_seed, use_deterministic_algos, cudnn_deterministic
     torch.use_deterministic_algorithms(use_deterministic_algos)
     torch.backends.cudnn.deterministic = cudnn_deterministic
     torch.backends.cudnn.benchmark = cudnn_benchmark
+
+
+def generate_numpy_seed_sequence(num_children=1, dtype=np.uint32, return_as='torch'):
+    root_seed_seq = np.random.SeedSequence() # entropy=None
+    entropy = root_seed_seq.entropy
+    children = root_seed_seq.spawn(num_children)
+
+    seeds = [child.generate_state(n_words=1, dtype=dtype)[0] for child in children]
+
+    if return_as == 'torch':
+        seeds = torch.from_numpy(seeds)
+    elif return_as != 'numpy':
+        raise ValueError(
+            f"Unrecognized value {return_as} for `return_as`. Must be 'torch' "
+            "or 'numpy'."
+        )
+    
+    return seeds, entropy, root_seed_seq, children
+
 
 
 # Could also set seed for random module and numpy if needed.
