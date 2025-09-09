213a752da12ad116c58e67ff8315edfbd02e4741
Jonathan Chien <jonathanmchien@gmail.com>
2025-09-09 11:44:41 -0500

    Commit in advance of run experiments/2025-09-09/0001

--- experiments/2025-09-09/0001/run.py
+++ experiments/2025-09-09/0001/run.py
@@ -1,22 +1,24 @@
+import os
+
 from pathlib import Path
 
-from datetime import datetime
 from engine.driver import run
 from general_utils import metadata as metadata_utils
 
 
 # ----------------------------- Set run args -------------------------------- #
 run_args = dict(
-    data_train_cfg_ref_list=['aaaa/0000/9999'],
-    model_cfg_ref_list=['aaaa/0001/0002'],
+    data_train_cfg_ref_list=['2025-09-05/o/0000'],
+    model_cfg_ref_list=['2025-09-09/a/0000'],
     pretrained_model_filepath_list=None,
-    training_cfg_ref_list=['aaaa/0001/0000'],
-    data_test_cfg_ref_list=['aaaa/0000/9999', 'aaaa/0000/9998'],
-    testing_cfg_ref_list=['aaaa/0001/0000'],
-    reproducibility_cfg_ref_list=['aa/0000'],
-    seed_idx_list=[0],
-    exp_group_id='aaaa',
-    exp_id='0002',
+    training_cfg_ref_list=['2025-09-05/a/0000'],
+    data_test_cfg_ref_list=['2025-09-05/o/0000', '2025-09-05/e/0000'],
+    cross_test=True,
+    testing_cfg_ref_list=['0000-00-00/a/0000'],
+    reproducibility_cfg_ref_list=['0000-00-00/a/0000'],
+    seed_idx_list=[0, 1, 2, 4, 5],
+    exp_date='2025-09-09',
+    exp_id='0001',
     run_id_suffix='',
     model_suffix='_best.pt',
     weights_only=False
@@ -27,12 +29,14 @@ exp_dir = Path(__file__).resolve().parent
 metadata_utils.collect_and_save_metadata(
     additional_info={'run_args': run_args, 'exp_dir': str(exp_dir)},
     filepath=exp_dir / 'metadata.json',
-    enforce_clean_git_tree=False,
+    enforce_clean_git_tree=True,
     overwrite=False
 )
 metadata_utils.create_textfile(
-    """\
-    Train on 5, 7, 9, test on 6, 8, 10 with 2 hidden units.
+    """
+    20 hidden unit Elman RNN with tanh nonlinearity and 0.5 dropout but 
+    no weight decay or other regularization. Train on odd sequence lengths in 
+    [1, 19], test on even lengths in [0, 20].
     """,
     filepath=exp_dir / 'README.md',
     dedent=True,
@@ -43,12 +47,9 @@ metadata_utils.create_textfile(
 training, testing, returned_exp_dir = run(**run_args)
 
 # Verify that experimental results were logged in the same directory as this script.
-if str(exp_dir) != ('/Users/jonathan/projects/counting_rnn/' + str(returned_exp_dir)):
+if str(exp_dir) != str(returned_exp_dir.resolve()):
     raise RuntimeError(
         "Mismatch between intended and actual location of experimental results.\n"
         f"- Expected: {exp_dir} (location of this script)\n"
         f"- Received: {returned_exp_dir} (returned by the run/run_curriculum function)\n\n"
     )
-
-
-
--- experiments/aaaa/0001/README.md
+++
@@ -1,1 +0,0 @@
-Train on 5, 7, 9, test on 6, 8, 10 with 3 hidden units.
